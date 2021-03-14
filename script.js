600b6b371350fafac53920f5017f88ee600053ed
alex888-gylyvatuy <80599292+alex888-gylyvatuy@users.noreply.github.com>
2021-03-14 19:38:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function heart_it (){
+  "use-strict";
+  document.getElementById("hearticon").style.backgroundImage = "url('https://cdn4.iconfinder.com/data/icons/e-commerce-icon-set/48/Like_2-48.png')";
+}
+
+function dontheart_it (){
+  "use-strict";
+  document.getElementById("hearticon").style.backgroundImage = "url('https://cdn4.iconfinder.com/data/icons/e-commerce-icon-set/48/Like-48.png')";
+}
+function follow (){
+  "use-strict";
+  document.getElementById("usericon").style.backgroundImage = "url('https://cdn4.iconfinder.com/data/icons/e-commerce-icon-set/48/Username_2-32.png')";
+}
+
+function dontfollow (){
+  "use-strict";
+  document.getElementById("usericon").style.backgroundImage = "url('https://cdn4.iconfinder.com/data/icons/e-commerce-icon-set/48/Username-48.png')";
+}
